6e3b78db6026bc483dedfb2b3d27da8ba6aa3bf5
Olgaa-Z <zelviaolgamaharani98@gmail.com>
2023-06-30 14:48:04 +0700

    Initial Commit

---
+++ TestIos/TestIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIosApp.swift
+//  TestIos
+//
+//  Created by visionet on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TestIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
